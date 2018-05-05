6d6fc64bc5d46e3716f5e4df654e2f94cd96f9a9
Simon Shi <simonsmh@gmail.com>
2018-05-05 20:51:56 +0800

    Bring up Webview

---
+++ ybqtloginui.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/login.ui'
+#
+# Created by: PyQt5 UI code generator 5.10
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_LoginWindow(object):
+    def setupUi(self, LoginWindow):
+        LoginWindow.setObjectName("LoginWindow")
+        LoginWindow.resize(501, 425)
+        self.centralwidget = QtWidgets.QWidget(LoginWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.webEngineView = QtWebEngineWidgets.QWebEngineView(self.centralwidget)
+        self.webEngineView.setUrl(QtCore.QUrl("https://www.yiban.cn/"))
+        self.webEngineView.setObjectName("webEngineView")
+        self.gridLayout.addWidget(self.webEngineView, 0, 0, 1, 1)
+        LoginWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(LoginWindow)
+        QtCore.QMetaObject.connectSlotsByName(LoginWindow)
+
+    def retranslateUi(self, LoginWindow):
+        _translate = QtCore.QCoreApplication.translate
+        LoginWindow.setWindowTitle(_translate("LoginWindow", "登录易班"))
+
+from PyQt5 import QtWebEngineWidgets
